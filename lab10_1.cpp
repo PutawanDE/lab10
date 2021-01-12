158f070c3fc0f8bfa5a34541fcc2751676476d75
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-01-12 16:30:51 +0700

    Finish lab10

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.ignore();
+    cin.ignore();
+    cin.ignore();
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    srand(time(0));
+    int r = rand() % 9;
+    cout << "You will get " << grades[r] << " in this 261102.";
+    return 0;
+}
